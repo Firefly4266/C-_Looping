8585e0bc239ee73b6f2044ea8a4e0a66ece41a5c
Firefly4266 <gaines.johnp@gmail.com>
2017-09-13 10:38:45 -0700

    Completed Michelle do while exercise

--- Looping/Program.cs
+++ Looping/Program.cs
@@ -10,6 +10,16 @@ namespace Looping
     {
         static void Main(string[] args)
         {
+            Console.Write("Greetings user, please enter a multi-digit whole number:  ");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+            var result = userInput * .1;
+            var x = Convert.ToInt32(result % 1 * 10);
+            Console.Write(x);
+            userInput = (int)result;
+            } while (userInput != 0);
+
         }
     }
 }
