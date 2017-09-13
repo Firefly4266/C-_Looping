ad881434a30f7de06dd165050f895bef122ca6be
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 17:02:04 -0700

    Started pyramid code.

--- Looping/Program.cs
+++ Looping/Program.cs
@@ -10,6 +10,18 @@ namespace Looping
     {
         static void Main(string[] args)
         {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int column = 5; column < row + 5; column++)
+                {
+                    for (var space = 5; space < 10; space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
