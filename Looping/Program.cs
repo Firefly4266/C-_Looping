ed23434408093549ada23767ae64ce4fb0a689b5
Firefly4266 <gaines.johnp@gmail.com>
2017-09-13 00:04:15 -0700

    Completed pyramid exercize.

--- Looping/Program.cs
+++ Looping/Program.cs
@@ -10,17 +10,17 @@ namespace Looping
     {
         static void Main(string[] args)
         {
-            for (int row = 0; row < 10; row++)
+            for (int row = 0; row < 11; row++)// this will give me the number of rows I will have and the row var serves as a position marker.
             {
-                for (int column = 5; column < row + 5; column++)
+                for (var space = 0; space < 11 - row; space +=2)// Here we need to insert spaces. The number of spaces will never be greater than 1/2 of the number of columns/rows(11) in relation to the row we are on...hence 11 - row. We accomplish this through incrimenting by two cutting the distance across the row in half.
                 {
-                    for (var space = 5; space < 10; space++)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.Write("*");
+                    Console.Write("x");// this is the actual space being printed out.  I substituted an 'x' for better illustration.
                 }
-                Console.WriteLine();
+                for (int column = 6; column < row + 6; column++)// This loop establishes the column position as in which column we are presently located. When the condition of the first loop is met, we drop into this loop.  We start at the midpoint because that is the furthest the first loop will go.
+                {
+                    Console.Write("*");// Here we will print until the condition is met.
+                }
+                Console.WriteLine();// This will move us to the next row.
             }
         }
     }
